85b86c9b4355c9a70ba25f16ac25566f3779c9d0
Patrick Betz <patrick.betz@awi.uni-heidelberg.de>
2017-12-01 22:49:53 +0100

    implemented basic question matching functionality

--- firsttest/models.py
+++ firsttest/models.py
@@ -18,7 +18,20 @@ class Constants(BaseConstants):
     players_per_group = None
     num_rounds = 1
 
-    correctwordlist = ['Apple', 'Banana']
+    #the first word in s_i is always the exactly correct solution
+    quizload = [{'question':"Name a Place You Visit Where You Aren't Allowed to Touch Anything",
+                   's1': ["museum gallery",'museum', 'gallery', 'museum gallery'],
+                   's2': ['zoo', 'animal'],
+                   's3': ["gentleman's club", 'gentleman club', 'stripclub', 'strip club'],
+                   's4': ['baseball'],
+                   's5': ['china shop']},
+
+                {'question': "Name an Article of Clothing You Can't Wash in the Washing Machine",
+                 's1': ['shoe','shoes'],
+                 's2': ['bra'],
+                 's3': ['hat'],
+                 's4': ['coat'],
+                 's5': ['sweater']}]
 
 
 
@@ -27,26 +40,49 @@ class Subsession(BaseSubsession):
     def creating_session(self):
         #TODO check but I think this is done for every round seperately, so you can use it
         #TODO to distribute the questions to all game rounds of all rounds of the experiment
-        alist = []
-        alist.append(random.choice(Constants.correctwordlist))
-        self.session.vars['correctwordlist'] = alist
+
+        question = random.choice(Constants.quizload)
+        #each quizload_i shall only hold one question so there shouldnt be ambiguity
+        self.session.vars['quizload1'] = question
+        #TODO add quizload 2-5 for the other 4 familifeud subrounds of one game round
 
 
 
 
 class Group(RedwoodGroup):
 
+    #TODO: when I enter the session with a player it does not wait for the other players to start yet?
+    #TODO but at least the function is called, this is good I suppose
+    def when_all_players_ready(self):
+        print('when all players ready was called...')
+        #send the question out to the channel
+        self.send('questionChannel', self.session.vars['quizload1']['question'])
+        return
+
+
     def _on_guessingChannel_event(self, event=None, **kwargs):
 
+        #TODO Delete me
         print('I went into "_on_guessing_Channel_events_" function...')
-        print('I received the guess of the player, the guess is: %s'  %(event.value['guess']))
+        print('I received the guess of the player, the guess is: %s' % (event.value['guess']))
+
+        #the guess that was send from a player
+        guess = event.value['guess'].lower()
+        # the the current question (dictionaire)
+        question = self.session.vars['quizload1']
+
+        good_guess = False
+        for answernum in ['s1', 's2', 's3', 's4', 's5']:
+            #check if the guess is correct, if yes send it, togehter with the answernum, to to correct_guess channel
+            if guess in question[answernum]:
+                good_guess = True
+                self.send('correct_guess', {'correctguess': guess.capitalize(),
+                                            'whichword': answernum})
+        if good_guess == False: #word was not right, send it to the usual group channel so it can be displayed on the right
+                self.send('group_guesses', guess)
 
 
-        #if the word is correct, send it to the correct_guess channel
-        if event.value['guess'] in self.session.vars['correctwordlist']:
-            self.send('correct_guess', event.value['guess'])
-        else: #if the word is not correct send it to the group_guesses channel
-            self.send('group_guesses', event.value['guess'])
+
 
 
 
